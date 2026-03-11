74ce8a3ddee3750f4a69183c34e327fca2429272
janbanan630-ops <janbanan630@gmail.com>
2026-03-11 13:12:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ document.querySelector(".go").addEventListener('click', () => {
     var firstname = document.querySelector(".firstname");
     var surname = document.querySelector(".surname");
     var image = document.querySelector(".image");
-    var date = document.querySelector(".date");
-    var number = document.querySelector(".number");
+    var birthdate = document.querySelector(".birthdate");
+    var pesel = document.querySelector(".pesel");
 
 
     var params = new URLSearchParams();
@@ -12,7 +12,8 @@ document.querySelector(".go").addEventListener('click', () => {
     params.set("surname", surname.value);
     params.set("image", image.value);
     location.href = "id.html?" + params;
-    params.set("date", date.value);
-    params.set("number", number.value);
+    params.set("birthdate", date.value);
+    params.set("pesel", number.value);
 });
+
 
